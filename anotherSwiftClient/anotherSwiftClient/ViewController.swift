e0cf31c60159142597abd8ce157006f22ef1d67a
Darius Jankauskas <darius.jankauskas@thefloow.com>
2020-06-30 18:06:19 +0100

    new project

---
+++ anotherSwiftClient/anotherSwiftClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  anotherSwiftClient
+//
+//  Created by Darius Jankauskas on 30/06/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
