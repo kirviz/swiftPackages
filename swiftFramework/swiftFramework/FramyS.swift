a9f8f756ab5bd737d31edf2c9a41db0947ff6e66
Darius Jankauskas <darius.jankauskas@thefloow.com>
2020-06-30 10:09:46 +0100

    initial commit

---
+++ swiftFramework/swiftFramework/FramyS.swift
@@ -0,0 +1,16 @@
+//
+//  FramyS.swift
+//  swiftFramework
+//
+//  Created by Darius Jankauskas on 30/06/2020.
+//
+
+import Foundation
+
+public class FramyS {
+    public let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
